4f33fe24b827af6492641fd5da87d284ce1dc5a3
Conny <conyperla1818@gmail.com>
2024-11-11 20:02:18 -0300

    Agregar nota nueva

--- app/src/main/java/com/example/notas/MainActivity.java
+++ app/src/main/java/com/example/notas/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.notas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     EditText notanueva;
@@ -19,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
         notanueva = findViewById(R.id.NotaNueva);
         agregar = findViewById(R.id.btnAgregar);
         Lista = findViewById(R.id.Notas);
+    }
+
+    public void AgregarNota(View view){
+        String nota = notanueva.getText().toString();
+        //Creo una lista
+        ArrayList<String> notas = new ArrayList<String>();
+
+        //Creo un adaptador para la lista con: contexto (this), android.R.layout.simple_list_item_1,nombre del array
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,notas);
+
+        //Asigno el adaptador a la lista ya definida
+        Lista.setAdapter(adaptador);
+
+        if (nota.isEmpty()){
+            Toast.makeText(this, "No puede ingresar una nota vacia", Toast.LENGTH_SHORT).show();
+        }else{
+            notas.add(nota);
+
+        }
     }
 
 }
