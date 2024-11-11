4a3ddaacd4d02a3483dec238c889d59edbefe0fd
Conny <conyperla1818@gmail.com>
2024-11-11 19:39:04 -0300

    Interfaz grafica

--- app/src/main/java/com/example/notas/MainActivity.java
+++ app/src/main/java/com/example/notas/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.notas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText notanueva;
+    Button agregar;
+    ListView Lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notanueva = findViewById(R.id.NotaNueva);
+        agregar = findViewById(R.id.btnAgregar);
+        Lista = findViewById(R.id.Notas);
     }
+
 }
